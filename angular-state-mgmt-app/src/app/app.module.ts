1fe5a425e935e6cc6e57a5f77896085828f5e705
Doug Corbett <dougccorbett@gmail.com>
2017-12-26 22:01:40 -0500

    Added house crud components, page-not-found, contact-us, header, footer.

--- angular-state-mgmt-app/src/app/app.module.ts
+++ angular-state-mgmt-app/src/app/app.module.ts
@@ -3,11 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HouseListComponent } from './components/houses/house-list/house-list.component';
+import { HouseDetailComponent } from './components/houses/house-detail/house-detail.component';
+import { HouseAddComponent } from './components/houses/house-add/house-add.component';
+import { HouseEditComponent } from './components/houses/house-edit/house-edit.component';
+import { HouseThumbnailComponent } from './components/houses/house-thumbnail/house-thumbnail.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HouseListComponent,
+    HouseDetailComponent,
+    HouseAddComponent,
+    HouseEditComponent,
+    HouseThumbnailComponent,
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-state-mgmt-app/src/app/components/houses/house-thumbnail/house-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-house-thumbnail',
+  templateUrl: './house-thumbnail.component.html',
+  styleUrls: ['./house-thumbnail.component.css']
+})
+export class HouseThumbnailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

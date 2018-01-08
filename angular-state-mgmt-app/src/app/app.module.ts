e4dea885f251dcae2fce7219a9a2c75f5a093107
Doug Corbett <dougccorbett@gmail.com>
2018-01-07 21:26:19 -0500

    remove house components

--- angular-state-mgmt-app/src/app/app.module.ts
+++ angular-state-mgmt-app/src/app/app.module.ts
@@ -4,11 +4,6 @@ import { Router, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HouseListComponent } from './components/houses/house-list/house-list.component';
-import { HouseDetailComponent } from './components/houses/house-detail/house-detail.component';
-import { HouseAddComponent } from './components/houses/house-add/house-add.component';
-import { HouseEditComponent } from './components/houses/house-edit/house-edit.component';
-import { HouseThumbnailComponent } from './components/houses/house-thumbnail/house-thumbnail.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
@@ -34,7 +29,6 @@ const appRoutes: Routes = [
   { pathMatch: 'full',  path: 'customers/:id', component: CustomerDetailComponent },
   { pathMatch: 'full',  path: 'customers/:id/edit', component: CustomerEditComponent },
   { pathMatch: 'full',  path: 'seed-data', component: SeedDataComponent },
-  { pathMatch: 'full',  path: 'houses', component: HouseListComponent },
   { pathMatch: 'full',  path: 'contact', component: ContactUsComponent },
   { pathMatch: 'full',  path: '**', component: PageNotFoundComponent }
 ]
@@ -42,11 +36,6 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     NavbarComponent,
-    HouseListComponent,
-    HouseDetailComponent,
-    HouseAddComponent,
-    HouseEditComponent,
-    HouseThumbnailComponent,
     FooterComponent,
     HomeComponent,
     PageNotFoundComponent,
